9029ae028870b01c9ee9557bbd9ad08ce64153c2
Theodor Abi-Mansour <theodor@abimansour.com>
2020-02-13 21:15:15 +0200

    Version Beta

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('WeAreHikers APP'),
-      centerTitle: true,
-      backgroundColor: Colors.redAccent.shade700,
-    ),
-    body: Center(
-      child: Text('Hello Theodor',style: TextStyle(
-        fontWeight: FontWeight.w900,
-        fontSize:35.0,
-        fontFamily: 'RobotoCondensed-Italic',
-      )),
-    ),
-    backgroundColor: Colors.blueGrey.shade100,
-    floatingActionButton: FloatingActionButton(
-      child: Text('+', style: TextStyle(
-        fontWeight: FontWeight.w900,
-        fontSize:35.0,
-        fontFamily: 'RobotoCondensed-Bold',
-      )),
-      backgroundColor: Colors.greenAccent.shade700,
-
-      onPressed: () {},
-    ),
-  ),
+  home: Home()
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WeAreHikers APP'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent.shade700,
+      ),
+
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text('hello'),
+          FlatButton(
+            onPressed: () => {},
+            color: Colors.grey[600],
+            child: Text('Press Me'),
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(15),
+            child: Text('inside container'),
+          ),
+        ],
+      ),
+
+      backgroundColor: Colors.blueGrey.shade100,
+      floatingActionButton: FloatingActionButton(
+        child: Text('+', style: TextStyle(
+          fontWeight: FontWeight.w900,
+          fontSize:25.0,
+          fontFamily: 'RobotoCondensed-Bold',
+        )),
+        backgroundColor: Colors.greenAccent.shade700,
+        onPressed: () {},
+      ),
+    );
+  }
+}
